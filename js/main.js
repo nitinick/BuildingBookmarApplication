906ac7dbc23fb8ca55e57648256ce10194285bc6
nick6064 <nitinyelwande6064@gmail.com>
2018-06-21 22:23:53 +0530

    delete and fecth function added

--- js/main.js
+++ js/main.js
@@ -32,12 +32,37 @@ bookmarks.push(bookMark);
 
 localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
 
-
+fetchbookmarks();
 }
 
  //prevent form from  submitting
  e.preventDefault();
 }
+//delete bookmaarks
+
+function deleteBookmark(url){
+
+  var bookmarks=JSON.parse(localStorage.getItem('bookmarks'));
+
+  for(var i=0;i<=bookmarks.length;i++){
+
+
+
+    if(bookmarks[i].url==url){
+
+      bookmarks.splice(i,1);
+    }
+  }
+  //re-set back to loaclstorage
+  localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+//refetch bookmarks
+
+
+}
+
+
+
+
 //fetch bookmarks
 function fetchbookmarks(){
 
@@ -56,7 +81,7 @@ function fetchbookmarks(){
     var url=bookmarks[i].url;
 
 
-    bookmarksResults.innerHTML +='<div class="well">'+'<h3>'+name+'<a class="btn btn-default" target="_blank" href="'+url+'">visit</a>'+'</h3>'+'</div>';
+    bookmarksResults.innerHTML +='<div class="well">'+'<h3>'+name+'<a class="btn btn-default" target="_blank" href="'+url+'">visit </a>'+'<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger"  href="#">Delete </a>'+'</h3>'+'</div>';
   }
 
 
